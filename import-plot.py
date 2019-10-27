2e2e2ba1a41ec84ffeddbfc1a3216ba53d4a50ae
SoyStillGood <zachzemokhol@gmail.com>
2019-10-27 14:59:38 -0400

    First Commit, pairs plot

---
+++ import-plot.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import argparse
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", dest="file", help="input data file")
+
+    args = parser.parse_args()
+    file = args.file
+
+    diabetes = pd.read_csv(filepath_or_buffer=file, sep=' ', header=0)
+    print(diabetes)
+
+    sns.pairplot(diabetes)
+    plt.show()
+# at this point we have the pair of columns scattered
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+# this is the implementation I've made after being paired up with
+# Thibault. Quite handy
